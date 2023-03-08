220a70f7d2e62d7eaefd669b512ec7146e8bbada
deforde <daniel.forde001@gmail.com>
2023-03-08 14:01:10 +0200

    finish p11

--- p11.lua
+++ p11.lua
@@ -44,6 +44,38 @@ local function generate_monkeys()
     return monkeys
 end
 
+local function sim_rounds(monkeys, nrounds, do_div)
+    local super_mod = 1
+    for _, m in ipairs(monkeys) do
+        super_mod = super_mod * m.test_quotient
+    end
+    for _ = 1, nrounds do
+        for _, m in ipairs(monkeys) do
+            while #m.items > 0 do
+                local item = table.remove(m.items)
+                m.inspect_cnt = m.inspect_cnt + 1
+                if m.op == "add" then
+                    item = item + m.op_val
+                elseif m.op == "mul" then
+                    item = item * m.op_val
+                else
+                    item = item * item
+                end
+                if do_div then
+                    item = math.floor(item / 3)
+                else
+                    item = item % super_mod
+                end
+                if item % m.test_quotient == 0 then
+                    table.insert(monkeys[m.true_target].items, item)
+                else
+                    table.insert(monkeys[m.false_target].items, item)
+                end
+            end
+        end
+    end
+end
+
 local function print_monkeys(monkeys)
     for i, m in ipairs(monkeys) do
         print("index: " .. i)
@@ -65,5 +97,22 @@ local function print_monkeys(monkeys)
     end
 end
 
-local monkeys = generate_monkeys()
-print_monkeys(monkeys)
+local function solve(nrounds, do_div)
+    local monkeys = generate_monkeys()
+    -- print_monkeys(monkeys)
+
+    sim_rounds(monkeys, nrounds, do_div)
+
+    local inspect_cnts = {}
+    for i, m in ipairs(monkeys) do
+        inspect_cnts[i] = m.inspect_cnt
+    end
+    table.sort(inspect_cnts)
+    return inspect_cnts[#inspect_cnts] * inspect_cnts[#inspect_cnts - 1]
+end
+
+local ans = solve(20, true)
+assert(ans == 102399)
+
+ans = solve(10000, false)
+assert(ans == 23641658401)
